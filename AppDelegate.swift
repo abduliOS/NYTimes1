a2300f8e4f8e7f276e884508975a4cce1e57565b
Abdul sathar <abdul.embedded@gmail.com>
2020-06-28 11:36:58 +0400

    Testing
    Testing

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: UISceneSession Lifecycl
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
